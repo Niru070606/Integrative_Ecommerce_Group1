8cfc16342b97a34b27023cceac0bba7a18f1e4c3
Niru070606 <neilalbertrondero67@gmail.com>
2026-05-03 21:01:08 +0800

    dinagdag ko yung sinabi ni sir

--- app.py
+++ app.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('public/main.html')
 
 @app.route('/secondPage')
 def secondPage():
-    return render_template('secondPage.html')
+    return render_template('public/secondPage.html')
 
 @app.route('/product')
 def product():
-    return render_template('product.html')
+    return render_template('public/product.html')
 
 @app.route('/deals')
 def deals():
-    return render_template('deals.html')
+    return render_template('public/deals.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('public/contact.html')
 
 @app.route('/product/<filename>')
 def product_detail(filename):
-    return render_template('product_templates/' + filename + '.html')
+    return render_template('public/product_templates/' + filename + '.html')
 """
 yung product/filename route web url
  ay kukunin nya lahat ng name ng file para ma rener yung exact file gamit yung string concatenation sa baba
