1f8d50d417bb6c09d196f468b2dc5412a7e5de39
Niru070606 <neilalbertrondero67@gmail.com>
2026-05-31 12:27:46 +0800

    update the entire system with flask

---
+++ app/models/ecommerce.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Ecommerce(db.Model):
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    product_description = db.Column(db.String(100), nullable=False)
+    file_path = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    stocks = db.Column(db.Integer, nullable=False)
+    specification = db.Column(db.String(2000), nullable=False)
+
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
